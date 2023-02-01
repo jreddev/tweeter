ef20864e5171b8c977fe34c8aa68b26b62555ba6
Jared <jared.devenport@gmail.com>
2023-01-31 21:52:49 -0700

    finished video lecture of refactoring. Milestone 2a Created FollowingPresenter and a Follow Service.

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/GetFollowingPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/GetFollowingPresenter.java
@@ -1,4 +1,77 @@
 package edu.byu.cs.tweeter.client.presenter;
 
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.model.domain.User;
+
 public class GetFollowingPresenter {
+
+    private static final int PAGE_SIZE = 10;
+    private User lastFollowee;
+
+    private boolean hasMorePages;
+
+    public boolean hasMorePages() {
+        return hasMorePages;
+    }
+
+    public void setHasMorePages(boolean hasMorePages) {
+        this.hasMorePages = hasMorePages;
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    private boolean isLoading = false;
+    public interface View {
+        void setLoadingFooter(boolean value);
+        void displayMessage(String message);
+
+        void addMoreItems(List<User> followees);
+    }
+
+    private View view;
+
+    private FollowService followService;
+
+    public GetFollowingPresenter(View view) {
+        this.view = view;
+        followService = new FollowService();
+    }
+
+    public void loadMoreItems(User user) {
+        if (!isLoading) {
+            isLoading = true;
+            view.setLoadingFooter(isLoading);
+            followService.loadMoreItems(user, PAGE_SIZE, lastFollowee, new GetFollowingObserver());
+        }
+    }
+
+    public class GetFollowingObserver implements FollowService.Observer {
+        @Override
+        public void displayError(String message) {
+            isLoading = false;
+            view.setLoadingFooter(isLoading);
+            view.displayMessage(message);
+        }
+
+        @Override
+        public void displayException(Exception ex) {
+            isLoading = false;
+            view.setLoadingFooter(isLoading);
+            view.displayMessage("Failed to get following because of exception: " + ex.getMessage());
+
+        }
+
+        @Override
+        public void addFollowees(List<User> followees, boolean hasMorePages) {
+            isLoading = false;
+            view.setLoadingFooter(isLoading);
+            lastFollowee = (followees.size() > 0) ? followees.get(followees.size() - 1) : null;
+            setHasMorePages(hasMorePages);
+            view.addMoreItems(followees);
+        }
+    }
 }
